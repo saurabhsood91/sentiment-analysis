e410f90a98a9a00954b3dcc9733f092f75b9c016
saurabhsood91 <saurabhsood91@gmail.com>
2015-11-05 01:22:26 -0700

    start classifier and split training and test set

--- src/classifier.py
+++ src/classifier.py
@@ -13,6 +13,17 @@ class NaiveBayesClassifier(object):
         self.filereader.ParseFile()
         self.negative_words = self.filereader.GenerateCounts()
         self.MergeCounts()
+
+        # Compute word count of positive words
+        self.positive_word_count = 0
+        self.negative_word_count = 0
+        for word, count in self.positive_words.iteritems():
+            self.positive_word_count += count
+        for word, count in self.negative_words.iteritems():
+            self.negative_word_count += count
+        # print self.positive_word_count
+        # print self.negative_word_count
+
     def MergeCounts(self):
         # set bag of words to initially be positive words
         self.bag_of_words = self.positive_words
@@ -24,4 +35,19 @@ class NaiveBayesClassifier(object):
             else:
                 # Add the word to the bag of words
                 self.bag_of_words[word] = count
-                print self.bag_of_words
+                # print self.bag_of_words
+        # Compute the total number of word types
+        self.total_word_type_count = len(self.bag_of_words.keys())
+        # print self.total_word_type_count
+    def Classify(self, test_file):
+        # create new filereader
+        # don't pass sentiment to the function. It will be set to None
+        test_file_reader = FileReader(test_file)
+        # Parse the file to read all the reviews
+        reviews = test_file_reader.ParseFile()
+
+        # Iterate over the reviews in the test set
+        for review in reviews.iteritems():
+            # print review
+            # split the review into words
+            pass
--- src/main.py
+++ src/main.py
@@ -2,3 +2,4 @@
 from classifier import NaiveBayesClassifier
 
 a = NaiveBayesClassifier("training_data/hotelPosT-train.txt","training_data/hoteNegT-train.txt")
+# a.Classify("training_data/hotelPosT-test.txt")
